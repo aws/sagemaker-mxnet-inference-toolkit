47f10c24d7557c0ba973f994d6b8aede6447a929
Dan Choi <choidan@amazon.com>
2019-05-15 21:28:51 -0700

    add serving main

---
+++ src/sagemaker_mxnet_serving_container/serving.py
@@ -0,0 +1,42 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+from __future__ import absolute_import
+
+import logging
+import os
+
+from sagemaker_inference import model_server
+
+import sagemaker_mxnet_serving_container.handler_service
+
+logger = logging.getLogger(__name__)
+
+HANDLER_SERVICE = sagemaker_mxnet_serving_container.handler_service.__name__
+
+DEFAULT_ENV_VARS = {
+    'MXNET_CPU_WORKER_NTHREADS': '1',
+    'MXNET_CPU_PRIORITY_NTHREADS': '1',
+    'MXNET_KVSTORE_REDUCTION_NTHREADS': '1',
+    'OMP_NUM_THREADS': '1',
+}
+
+
+def _update_mxnet_env_vars():
+    for k, v in DEFAULT_ENV_VARS.items():
+        if k not in os.environ:
+            os.environ[k] = v
+
+
+def main():
+    _update_mxnet_env_vars()
+    model_server.start_model_server(handler_service=HANDLER_SERVICE)
