75387d0f8cd9e3cc8170dc23a4c0f883eb12d0b0
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-06-20 10:25:43 -0700

    Change entrypoint

--- docker/1.4.1/mms-entrypoint.sh
+++ docker/1.4.1/mms-entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [[ "$1" = "serve" ]]; then
-    shift 1
-    mxnet-model-server --start --mms-config /home/model-server/config.properties
+    # shift 1
+    # mxnet-model-server --start --mms-config /home/model-server/config.properties
+    python -c "from sagemaker_mxnet_serving_container import serving; serving.main()"
 else
     eval "$@"
 fi
