a894d875b8845d189a5428780600086a035d79df
Arjuna Keshavan <arjunake@amazon.com>
2020-01-02 13:18:46 -0800

    use different version for ei

--- scripts/build_all.py
+++ scripts/build_all.py
@@ -24,6 +24,7 @@ def _parse_args():
 
     parser.add_argument('--account')
     parser.add_argument('--version')
+    parser.add_argument('--eia-version', default=None)
     parser.add_argument('--repo')
     parser.add_argument('--region', default=DEFAULT_REGION)
 
@@ -50,25 +51,33 @@ def _build_image(build_dir, arch, prev_image_uri):
     os.chdir(prev_dir)
 
 
-args = _parse_args()
+def main():
+    args = _parse_args()
 
-root_build_dir = os.path.join('docker', args.version)
+    root_build_dir = os.path.join('docker', args.version)
 
-# Run docker-login so we can pull the cached image
-get_login_cmd = 'aws ecr get-login --no-include-email --region {} --registry-id {}'.format(args.region, args.account)
-login_cmd = subprocess.check_output(get_login_cmd.split())
-print('Executing docker login command: '.format(login_cmd))
-subprocess.check_call(login_cmd.split())
+    # Run docker-login so we can pull the cached image
+    get_login_cmd = 'aws ecr get-login --no-include-email --region {} --registry-id {}'.format(args.region, args.account)
+    login_cmd = subprocess.check_output(get_login_cmd.split())
+    print('Executing docker login command: '.format(login_cmd))
+    subprocess.check_call(login_cmd.split())
 
-for arch in ['cpu', 'gpu', 'eia']:
-    for py_version in ['2.7', '3.6']:
-        tag_arch = 'cpu' if arch == 'eia' else arch
-        tag = '{}-{}-py{}'.format(args.version, tag_arch, py_version[0])
+    eia_version = args.eia_version if args.eia_version else args.version
 
-        repo = '{}-eia'.format(args.repo) if arch == 'eia' else args.repo
-        dest = '{}:{}'.format(repo, tag)
+    for arch in ['cpu', 'gpu', 'eia']:
+        for py_version in ['2.7', '3.6']:
+            tag_arch = 'cpu' if arch == 'eia' else arch
+            framework_version = eia_version if arch == 'eia' else args.version
+            tag = '{}-{}-py{}'.format(framework_version, tag_arch, py_version[0])
 
-        prev_image_uri = '{}.dkr.ecr.{}.amazonaws.com/{}'.format(args.account, args.region, dest)
+            repo = '{}-eia'.format(args.repo) if arch == 'eia' else args.repo
+            dest = '{}:{}'.format(repo, tag)
 
-        build_dir = os.path.join(root_build_dir, 'py{}'.format(py_version[0]))
-        _build_image(build_dir, arch, prev_image_uri)
+            prev_image_uri = '{}.dkr.ecr.{}.amazonaws.com/{}'.format(args.account, args.region, dest)
+
+            build_dir = os.path.join(root_build_dir, 'py{}'.format(py_version[0]))
+            _build_image(build_dir, arch, prev_image_uri)
+
+
+if __name__ == '__main__':
+    main()
