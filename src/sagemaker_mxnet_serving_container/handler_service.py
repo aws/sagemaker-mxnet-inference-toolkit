38d5cbad872bfc78f2d82e7680d98c0fa6b1958e
Qian Chen <chqa@amazon.com>
2021-04-02 14:23:28 -0700

    add logging

--- src/sagemaker_mxnet_serving_container/handler_service.py
+++ src/sagemaker_mxnet_serving_container/handler_service.py
@@ -13,6 +13,7 @@
 from __future__ import absolute_import
 
 import importlib
+import logging
 import os
 
 import mxnet as mx
@@ -25,6 +26,8 @@ from sagemaker_mxnet_serving_container.default_inference_handler import DefaultG
 from sagemaker_mxnet_serving_container.mxnet_module_transformer import MXNetModuleTransformer
 
 PYTHON_PATH_ENV = "PYTHONPATH"
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 class HandlerService(DefaultHandlerService):
@@ -50,6 +53,7 @@ class HandlerService(DefaultHandlerService):
             spec = importlib.util.spec_from_file_location(module_name, inference_script)
             user_module = importlib.util.module_from_spec(spec)
         else:
+            log.info("Please include /code/{}.py in model_dir".format(module_name))
             raise ValueError("Could not find valid inference_script path {} in model artifact".format(inference_script))
 
         if hasattr(user_module, 'transform_fn'):
