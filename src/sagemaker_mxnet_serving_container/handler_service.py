366298342e29ea174fceb820ea1e2e48844452b3
David Thomas <dthmasp@amazon.com>
2021-12-15 17:34:41 -0800

    Bump MMS version to 1.1.5 for all mxnet versions >=1.5.1 and update name to "multi-model-server"

--- src/sagemaker_mxnet_serving_container/handler_service.py
+++ src/sagemaker_mxnet_serving_container/handler_service.py
@@ -38,7 +38,7 @@ class HandlerService(DefaultHandlerService):
         - The ``handle`` method is invoked for all incoming inference requests to the model server.
         - The ``initialize`` method is invoked at model server start up.
 
-    Based on: https://github.com/awslabs/mxnet-model-server/blob/master/docs/custom_service.md
+    Based on: https://github.com/awslabs/multi-model-server/blob/master/docs/custom_service.md
 
     """
     def __init__(self):
